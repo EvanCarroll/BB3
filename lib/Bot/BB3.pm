11be29c4fa8d40aa00282ecebca442a0133d9e4d
Evan Carroll <me@evancarroll.com>
2009-10-23 12:56:47 -0500

    added makefile!!!

--- lib/Bot/BB3.pm
+++ lib/Bot/BB3.pm
@@ -12,6 +12,8 @@ use Bot::BB3::PluginManager;
 use Bot::BB3::PluginConfigParser;
 use Bot::BB3::Logger;
 
+our $VERSION = '3.01';
+use 5.008;
 
 use Data::Dumper;
 use strict;
@@ -278,3 +280,94 @@ sub SIGINT {
 }
 
 1;
+__END__
+
+=head1 NAME
+
+Bot::BB3 - The Robert Grimes Buubot with less suck
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Bot::BB3;
+
+    my $foo = Bot::BB3->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Robert Grimes, C<< <buu at erxz.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-bot-bb3 at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Bot-BB3>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Bot::BB3
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Bot-BB3>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Bot-BB3>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Bot-BB3>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Bot-BB3/>
+
+=back
+
+=head1 ACKNOWLEDGEMENTS
+
+Evan Carroll, for removing some major suck
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Robert Grimes.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
